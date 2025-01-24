be37a89482927846d2f5f87d2b8c5c8657c6438e
Mikelele <d.michaelthomasbennett@gmail.com>
2025-01-24 11:10:14 +0100

    Add button added to main gui

--- App_1/gui.py
+++ App_1/gui.py
@@ -3,14 +3,29 @@ import FreeSimpleGUI as sg
 
 #Instances of elements / widgets
 label = sg.Text('Type in a To-Do')
-input_box = sg.Input(tooltip="Enter To-Do")
+input_box = sg.Input(tooltip="Enter To-Do", key="todo")
 add_button = sg.Button("Add")
 
 #Instance of a window, mother of widget instances. All items in layout list of lists MUST be gui widgets
-window = sg.Window('My To-Do App', [[label], [input_box, add_button]])
+window = sg.Window('My To-Do App',
+                   [[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
 
-#Mthods on the instance
-window.read()
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+        case "Edit":
+            
+
 window.close()
 
 
