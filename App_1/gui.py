c762b8053712201d057d1c8a2eb018d2f4c3a5a9
Mikelele <d.michaelthomasbennett@gmail.com>
2025-01-27 09:36:02 +0100

    Day 17 done

--- App_1/gui.py
+++ App_1/gui.py
@@ -6,9 +6,15 @@ label = sg.Text('Type in a To-Do')
 input_box = sg.Input(tooltip="Enter To-Do", key="todo")
 add_button = sg.Button("Add")
 
+list_box = sg.Listbox(values=functions.get_todos(),
+                      key="todos",
+                      enable_events=True,
+                      size=[45, 10])
+edit_button = sg.Button("Edit")
+
 #Instance of a window, mother of widget instances. All items in layout list of lists MUST be gui widgets
 window = sg.Window('My To-Do App',
-                   [[label], [input_box, add_button]],
+                   [[label], [input_box, add_button], [list_box, edit_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -21,10 +27,22 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo + "\n"
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
+
         case sg.WIN_CLOSED:
             break
-        case "Edit":
-            
 
 window.close()
 
--- bonus_exercises/bonus_functions/bonus_gui_zip.py
+++ bonus_exercises/bonus_functions/bonus_gui_zip.py
@@ -1,21 +1,39 @@
 import FreeSimpleGUI as sg
-import shutil
+import bonus_gui_zip_creator
+
+from bonus_exercises.bonus_functions.bonus_gui_zip_creator import make_archive
 
 label_from = sg.Text("Select Files")
 input_from = sg.Input(tooltip="")
-button_from_choose = sg.FileBrowse("Choose Files")
+button_from_choose = sg.FilesBrowse("Choose", key="files")
 
 label_to = sg.Text("Select Folder")
 input_to = sg.Input(tooltip="route to")
-button_to_choose = sg.FolderBrowse("Choose Folder")
+button_to_choose = sg.FolderBrowse("Choose", key="folder")
 
-button_compress = sg.Button('compress')
+button_compress = sg.Button('Compress')
+output_label = sg.Text('', key="output")
 
 
 window = sg.Window("My compressor App",
                    layout=[[label_from, input_from, button_from_choose],
                            [label_to, input_to, button_to_choose],
-                           [button_compress]])
+                           [button_compress, output_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+    filepaths = values['files'].split(";")
+    folder = values['folder']
+    print(filepaths, folder)
+    match event:
+
+        case "Compress":
+            try:
+                make_archive(filepaths, folder)
+                window['output'].update('Your files have been compressed')
+            except:
+                output_label = "There have been an error"
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
